716b1ad09728cc7f9d6f594bea7e54991116acb6
SmileyNeema <joyirosh@gmail.com>
2025-05-16 22:36:34 -0700

    practice js

---
+++ practice.js
@@ -0,0 +1,101 @@
+//variables-data types  integers, boolean, string
+//arrays -  arrays methods- shift,unshift,pop,push iterations
+//functions
+//class - super class, inheritance
+//context
+//dome
+//use of let
+//use of const
+
+// typeof ()
+// console.log()
+//map
+// const name="sam"
+// const capitalName = 
+// console.log(capitalName)
+
+// const fruits = ["banana", "apple", "mango"];
+// const upperFruits = fruits.map(fruits => fruits.toUpperCase())
+// upperFruits.sort()
+// upperFruits.push("olive")
+// console.log(upperFruits)
+// sorting fruits.sort()
+// removes the first item fruits.pop()
+// adds at the end fruits.push("avocado")
+// removes firts fruits.shift()
+//add at the first fruits.unshift("pinneaple")
+
+//map uppecase
+//indices of arrays 
+//arrays changing values 
+//checking array length
+//arrays methods - add- .push()
+                //remove from the end - .pop()
+                // add in the beginning - .unshift()
+                //remove from the beginning - .shift()
+                //index of the element - indexOf()
+
+//looping arrays -
+                    //for loop
+                    //for of loop
+                    //forEach method
+
+//Advanced methods -
+                    //map()
+                    //filter()
+                    //find ()
+                    //includes () returns true or false
+                    //sort ()
+
+//ARRAYS 
+
+// const names = ["sam", "joy", 2]
+// console.log(names)
+
+//ARRAYS METHODS
+//1.MAP
+//to uppercase
+// const names = ["sam", "joy", "seth"]
+// const namesCapital = names.map(names => names.toUpperCase())
+// console.log(namesCapital)
+
+// const capNames = names.map(names => names.toUpperCase())
+// console.log(capNames)
+
+//2.INDECES OF ARRAYS - SHOWS POSITIONS OF ELEMENTS IN AN ARRAY 
+//by use of length
+// const names = ["sam", "joy", "seth"]
+// console.log(names[5])
+
+//3.Arrays changing values manipulating data 
+// const names = ["sam", "joy", "seth"]
+// names[0] = "Shiku"
+//  console.log(names)
+
+//4.checking array length - .length
+// const names = ["sam", "joy", "seth"]
+// console.log(names.length)
+
+//5. arrays methods - add- .push() -adds at the end
+// const names = ["sam", "joy", "seth"]
+// names.push("kayleigh")
+// console.log(names)
+
+//remove from the end - .pop()
+// const names = ["sam", "joy", "seth"]
+// names.pop()
+// console.log(names)
+
+// add in the beginning - .unshift()
+// const names = ["sam", "joy", "seth"]
+// names.unshift("kayleigh")
+// console.log(names)
+
+//remove from the beginning - .shift()
+// const names = ["sam", "joy", "seth"]
+// names.shift()
+// console.log(names)
+
+//index of the element - indexOf()
+// const names = ["sam", "joy", "seth"]
+// console.log(names.indexOf("joy"))
